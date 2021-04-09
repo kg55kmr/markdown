97e700917a692ec324b63053e24bf6ea07f787d6
secondary-school55 <kzh55@ukr.net>
2021-04-09 15:20:27 +0300

    update

--- src/markdown.svelte
+++ src/markdown.svelte
@@ -15,17 +15,35 @@
   import Ul from "./components/ul.svelte";
   import Li from "./components/li.svelte";
   import A from "./components/a.svelte";
+  import Img from "./components/img.svelte";
+  import Blockquote from "./components/blockquote.svelte";
+  import Em from "./components/em.svelte";
+  import Strong from "./components/strong.svelte";
+  import Hr from "./components/hr.svelte";
 
   export let source;
   export let components = {};
 
   $: ast = parser(source).children;
-  $: defaultComponents = { p: P, ol: Ol, ul: Ul, li: Li, a: A };
-  $: allComponents = {
-    ...defaultComponents,
-    ...globalComponents,
-    ...components,
+  $: defaultComponents = {
+    p: P,
+    ol: Ol,
+    ul: Ul,
+    li: Li,
+    a: A,
+    img: Img,
+    blockquote: Blockquote,
+    em: Em,
+    strong: Strong,
+    hr: Hr,
   };
 </script>
 
-<Parser {ast} components={allComponents} />
+<Parser
+  {ast}
+  components={{
+    ...defaultComponents,
+    ...globalComponents,
+    ...components,
+  }}
+/>
